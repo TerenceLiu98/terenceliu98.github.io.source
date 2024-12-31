6fd4fd0019a3fc3fdc0c185e5b1072d8e446367e
TerenceLau <Terenceliu1012@gmail.com>
2024-12-31 17:12:50 +0000

    rebuilding site Tue Dec 31 17:12:50 UTC 2024

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 find . -name "*.DS_Store" -delete
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+#hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 
 # update bibliography
